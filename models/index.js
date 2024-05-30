a0840711a7e17f0a5f2e4c856490c4822983f06b
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-05-30 16:37:58 -0700

    completed model files

--- models/index.js
+++ models/index.js
@@ -3,14 +3,37 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { castArray } = require('lodash');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey:'category_id', 
+  as: 'category', 
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'category_id', 
+  as: 'products',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag, 
+    unique: false
+  }, 
+  as: 'product_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag, 
+    unique: false
+  }, 
+  as: 'tagsForProducts'
+})
 
 module.exports = {
   Product,
